451aa3b683d536ef386ea1bc8c998e5e698cacb5
charu <charunegi409@gmail.com>
2023-03-05 16:10:19 +0530

    adding cors dependencies

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const { connect } = require("./config/db");
 const { HospitalRouter } = require("./routes/Hospital.route");
+const cors=require("cors")
 const app = express();
 app.use(express.json());
 app.use("/", HospitalRouter);
 require("dotenv").config();
-
+app.use(cors({
+  origin:"*"
+}))
 
 
 // app.use(bodyParser.urlencoded({ extended: true }))
